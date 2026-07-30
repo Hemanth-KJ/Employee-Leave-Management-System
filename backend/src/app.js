76ae452a227b5ffe137bed559c6bee3d49942665
Hemanth-KJ <hemanthdevanga48@gmail.com>
2026-07-30 20:45:47 +0530

    feat: design database and coonect supabase postgres

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const pool = require("./config/db");
 
 const app = express();
 
-const PORT =5000;
+const PORT =process.env.PORT || 5000;
 
 app.use(express.json());
 
@@ -12,6 +13,25 @@ app.get("/",(req, res)=> {
         message: "Employee Leave Managment API is running",
     });
 });
+
+app.get("/api/health/db", async (req, res)=>{
+    try{
+        const result = await pool.query("SELECT NOW()");
+
+        res.json({
+            sucess: true,
+            message: "Database connection successful",
+            databaseTime:result.rows[0].now
+,        });
+    } catch(error){
+        console.error("Database connection error:", error);
+
+         res.json({
+            sucess: false,
+            message: "Database connection failed",
+        });
+    }
+})
 
 app.listen(PORT, ()=>{
     console.log(`server running on http://localhost:${PORT}`);
